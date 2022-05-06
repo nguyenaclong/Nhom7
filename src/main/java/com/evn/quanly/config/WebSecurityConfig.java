833f07114928106cd4d9db9fe6896f3b4b326306
nguyenaclong <nguyenaclong@gmail.com>
2022-05-06 11:37:18 +0700

    Sign In Fix + Test JMeter

--- src/main/java/com/evn/quanly/config/WebSecurityConfig.java
+++ src/main/java/com/evn/quanly/config/WebSecurityConfig.java
@@ -60,6 +60,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
+                .loginPage("/login")
                 .usernameParameter("email")
                 .defaultSuccessUrl("/users")
                 .permitAll()
